8b259d47eb737966bf2491ab3d6f48e7736bb08f
CodeSandbox <support@codesandbox.io>
2023-12-28 20:28:39 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import "./styles.css";
+import React, { useState, useEffect } from "react";
+import "./styles.css"; // Import the CSS file
 
-export default function App() {
+const NewsApp = () => {
+  const [newsData, setNewsData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiKey = "06b89d08762d56a13cb3b24ca1887455"; // Replace with your GNews API key
+        const apiUrl = `https://gnews.io/api/v4/top-headlines?country=us&token=${apiKey}`;
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        setNewsData(data);
+      } catch (error) {
+        console.error("Error fetching news data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <div className="header">
+        <p>TeamLitho - News</p>
+      </div>
+      <h1>Top Headlines</h1>
+      {newsData && (
+        <ul>
+          {newsData.articles.map((article, index) => (
+            <li key={index}>
+              <h2>{article.title}</h2>
+              <p>{article.description}</p>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                Read more
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
+
+export default NewsApp;
